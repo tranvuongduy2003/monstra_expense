7f6519b7a648b3610f5cae56cae433f0f3bea720
Trần Ngọc Nhật Vy <21520536@gm.uit.edu.vn>
2023-03-27 16:19:48 +0700

    update tailwind config

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        // root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          tests: ['./tests/'],
-          '@components': './components',
-          '@screens': './screens',
-          '@features': './features',
-        },
-      },
-    ],
-  ],
+  plugins: ["nativewind/babel"],
 };
