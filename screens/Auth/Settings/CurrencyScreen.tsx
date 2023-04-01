2ffdd4f7df973c5de822e0108fee102ea95e640c
Trần Ngọc Nhật Vy <21520536@gm.uit.edu.vn>
2023-04-02 02:11:36 +0700

    TT TT ded

--- screens/Auth/Settings/CurrencyScreen.tsx
+++ screens/Auth/Settings/CurrencyScreen.tsx
@@ -1,22 +1,48 @@
 import React, {ReactElement} from 'react';
-import {StyleSheet, View, Text, Button, TouchableOpacity, FlatList} from 'react-native';
+import { useState } from 'react';
+import {StyleSheet, View, Text, Button, TouchableOpacity, FlatList, ListRenderItem} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {AppColors} from 'constants/AppColors';
 import scale from 'constants/Responsive';
 import CheckedIcon from 'assets/svg/CheckedIcon';
+import { Currency, CurrencyDATA } from './indexCurrency';
 
 interface ICurrencyScreenProps {
+    item: Currency;
     onPress?: () => void;
     children?: ReactElement;
 }
 
 const CurrencyScreen: React.FunctionComponent<ICurrencyScreenProps> = ({
-    onPress = () => {},
-    children,
 }) => {
+    const [selectedId, setSelectedId] = useState<string>();
+
+    const renderItem = ({item}: {item: Currency}) => {
+        return (
+            <TouchableOpacity 
+            style={styles.settingButton} 
+            onPress={() => {}}>
+            <View style={styles.content}>
+                <Text style={styles.title}>{item?.title}</Text>
+                {selectedId === item.id ? 
+                <View style={styles.icon}>
+                    <CheckedIcon></CheckedIcon>
+                </View> : null}
+          </View>
+        </TouchableOpacity>
+        );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-        <View style={styles.settingContainer}></View>
+        <View style={styles.settingContainer}>
+            <FlatList
+                data={CurrencyDATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                extraData={selectedId}
+            ></FlatList>
+        </View>
         <View style={styles.blankSpace}></View>
     </SafeAreaView>
   );
