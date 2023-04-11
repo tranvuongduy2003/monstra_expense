a61b9f0a2e9a831c5995d3c532bc0637984e335c
Trần Ngọc Nhật Vy <21520536@gm.uit.edu.vn>
2023-04-11 12:15:12 +0700

    eoeoeoeo

---
+++ screens/Settings/ExportNotiScreen.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {StyleSheet, View, Text} from 'react-native';
+import {AppColors} from 'constants/AppColors';
+import scale from 'constants/Responsive';
+import CustomDropDown from 'components/CustomDropDown';
+import {LanguageDATA} from './indexLanguage';
+import AppButton from 'components/AppButton';
+
+interface IExportNotiScreenProps {}
+const ExporNotitScreen: React.FunctionComponent<IExportNotiScreenProps> = ({}) => {
+  return (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.topContainer}></View>
+        <View style={styles.bottomContainer}></View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: AppColors.white,
+  },
+  topContainer: {
+    flex: 446,
+    backgroundColor: 'red',
+  },
+  bottomContainer: {
+    flex: 332,
+    backgroundColor: 'yellow',
+  },
+});
+
+export default ExporNotitScreen;
--- screens/Settings/ExportScreen.tsx
+++ screens/Settings/ExportScreen.tsx
@@ -1,38 +1,46 @@
-import React, {useState} from 'react';
+import React, {ReactElement} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import {AppColors} from 'constants/AppColors';
 import scale from 'constants/Responsive';
 import CustomDropDown from 'components/CustomDropDown';
 import {LanguageDATA} from './indexLanguage';
-import AppButton from 'components/AppButton';
+import ExportDataIcon from 'assets/svg/ExportDataIcon';
 
-interface IExportScreenProps {}
-const ExportScreen: React.FunctionComponent<IExportScreenProps> = ({}) => {
+interface IExportScreenProps {
+  title?: string;
+  onPress?: () => void;
+  children?: ReactElement;
+}
+const ExportScreen: React.FunctionComponent<IExportScreenProps> = ({
+  title,
+  onPress = () => {},
+  children,
+}) => {
   return (
     <SafeAreaView style={styles.container}>
-        <View style={styles.content}>
-          <CustomDropDown
-            data={LanguageDATA}
-            text="What data do your want to export?"></CustomDropDown>
-        </View>
-        <View style={styles.content}>
-          <CustomDropDown
-            data={LanguageDATA}
-            text="When date range?"></CustomDropDown>
-        </View>
-        <View style={styles.content}>
-          <CustomDropDown
-            data={LanguageDATA}
-            text="What format do you want to export?"></CustomDropDown>
-        </View>
+      <View style={styles.content}>
+        <CustomDropDown
+          data={LanguageDATA}
+          text="What data do your want to export?"></CustomDropDown>
+      </View>
+      <View style={styles.content}>
+        <CustomDropDown
+          data={LanguageDATA}
+          text="When date range?"></CustomDropDown>
+      </View>
+      <View style={styles.content}>
+        <CustomDropDown
+          data={LanguageDATA}
+          text="What format do you want to export?"></CustomDropDown>
+      </View>
       <View style={styles.bottomContainer}>
-        <View style={styles.buttonContainer}>
-          <AppButton
-            title="Back To Home"
-            backgroundColor={AppColors.primaryColor}
-            onPress={() => {}}></AppButton>
-        </View>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <View style={styles.titleContainer}>
+            <ExportDataIcon></ExportDataIcon>
+            <Text style={styles.title}>Export</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -57,11 +65,25 @@ const styles = StyleSheet.create({
     flexDirection: 'column-reverse',
     marginBottom: scale(50),
   },
-  buttonContainer: {
+  button: {
     width: scale(343),
     height: scale(56),
+    borderRadius: 16,
+    alignItems: 'center',
+    backgroundColor: AppColors.primaryColor,
+  },
+  titleContainer: {
     alignContent: 'center',
-    verticalAlign: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginVertical: scale(16),
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: AppColors.white,
+    marginLeft: scale(10),
   },
 });
 
