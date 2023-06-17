839eeccf057ea76e63d62470083e64f204bffabe
Trần Vương Duy <136442876+tranvuongduy@users.noreply.github.com>
2023-06-17 21:06:32 +0700

    code change

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import {store} from 'app/store';
-import React from 'react';
-import {Provider} from 'react-redux';
-
 import {AppNavigation} from 'navigation/AppNavigation';
 import {AuthProvider} from 'providers/AuthProvider';
+import React from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Provider} from 'react-redux';
 
 interface IAppProps {}
 
